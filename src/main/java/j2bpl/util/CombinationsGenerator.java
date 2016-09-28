6a9280a7c81a4b37225f88b9bed9e0ddaa2be132
Patricio Palladino <email@patriciopalladino.com>
2016-09-28 10:41:52 -0300

    Add a very inefficient combinations generator

---
+++ src/main/java/j2bpl/util/CombinationsGenerator.java
@@ -0,0 +1,43 @@
+package j2bpl.util;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class CombinationsGenerator<E> {
+
+    public Set<Set<E>> combinations(Set<E> elements) {
+
+        final ImmutableList<E> asList = ImmutableList.copyOf(elements);
+
+        final Set<Set<E>> combinations = new HashSet<>();
+
+        combinations.add(new HashSet<E>());
+
+        for (int i = 0; i < elements.size(); i++) {
+
+            final List<E> withoutI = listWithoutI(asList, i);
+
+            final HashSet<E> setWithoutI = new HashSet<>(withoutI);
+
+            for (Set<E> combination : combinations(setWithoutI)) {
+                combination.add(asList.get(i));
+                combinations.add(combination);
+            }
+
+        }
+
+        return combinations;
+    }
+
+    private List<E> listWithoutI(List<E> asList, int i) {
+        final ArrayList<E> copy = Lists.newArrayList(asList);
+        copy.remove(i);
+        return copy;
+    }
+
+}

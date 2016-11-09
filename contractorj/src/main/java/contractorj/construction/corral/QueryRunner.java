2947e3c18a2006006b9664434954d84f7af42cce
Patricio Palladino <email@patriciopalladino.com>
2016-11-09 16:08:10 -0300

    Improve QueryRunner's log.

--- contractorj/src/main/java/contractorj/construction/corral/QueryRunner.java
+++ contractorj/src/main/java/contractorj/construction/corral/QueryRunner.java
@@ -1,18 +1,39 @@
 package contractorj.construction.corral;
 
-import contractorj.model.Transition;
-import contractorj.model.Epa;
+import com.google.common.base.Joiner;
 import contractorj.construction.Query;
+import contractorj.model.Action;
+import contractorj.model.Epa;
+import contractorj.model.Transition;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 public class QueryRunner implements Runnable {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    public static final String ANSI_BLACK = "\u001B[30m";
+
+    public static final String ANSI_RED = "\u001B[31m";
+
+    public static final String ANSI_GREEN = "\u001B[32m";
+
+    public static final String ANSI_YELLOW = "\u001B[33m";
+
+    public static final String ANSI_BLUE = "\u001B[34m";
+
+    public static final String ANSI_PURPLE = "\u001B[35m";
+
+    public static final String ANSI_CYAN = "\u001B[36m";
+
+    public static final String ANSI_WHITE = "\u001B[37m";
 
     private final File boogieFile;
 
@@ -66,16 +87,55 @@ public class QueryRunner implements Runnable {
                 }
 
                 if (result.equals(Result.MAYBE_BUG)) {
-                    final Transition transition = new Transition(query.getFrom(), query.getTo(), query.getTransition(), true);
+                    final Transition transition = new Transition(query.getSource(), query.getTarget(), query.getTransition(), true);
                     epa.addEdge(transition);
                 }
 
                 final long lastRunTime = corralRunner.getLastRunTime();
 
-                final float v = lastRunTime / 1_000_000_000f;
+                final float time = lastRunTime / 1_000_000_000f;
 
-                System.out.println(queryName + ": " + result + " (t: " + v + "s - thread: " + Thread.currentThread().getName() + ")");
+                final ArrayList<String> sourceActions = new ArrayList<>();
+                for (final Action enabledAction : query.getSource().enabledActions) {
+                    sourceActions.add(enabledAction.method.getJavaNameWithArgumentTypes());
+                }
+
+                final ArrayList<String> targetActions = new ArrayList<>();
+                for (final Action enabledAction : query.getTarget().enabledActions) {
+                    sourceActions.add(enabledAction.method.getJavaNameWithArgumentTypes());
+                }
+
+                final StringBuilder logBuilder = new StringBuilder()
+                        .append("Query\n")
+                        .append("\tsource: ").append(Joiner.on(", ").join(sourceActions)).append("\n")
+                        .append("\ttransition: ").append(query.getTransition().method.getJavaNameWithArgumentTypes())
+                        .append("\n")
+                        .append("\ttarget: ").append(Joiner.on(", ").join(targetActions)).append("\n")
+                        .append("\tresult: ").append(result).append("\n")
+                        .append("\ttime: ").append(time).append("\n")
+                        .append("\tfile: ").append(boogieFile.getAbsolutePath()).append("\n")
+                        .append("\tprocedure: ").append(queryName).append("\n")
+                        .append("\n");
+
+                switch (result) {
+
+                    case TRUE_BUG:
+                        System.out.println(ANSI_GREEN + logBuilder.toString() + ANSI_RESET);
+                        break;
+
+                    case MAYBE_BUG:
+                        System.out.println(ANSI_BLUE + logBuilder.toString() + ANSI_RESET);
+                        break;
+
+                    case APPLICATION_BUG:
+                        System.out.println(ANSI_RED + logBuilder.toString() + ANSI_RESET);
+                        break;
+
+                    default:
+                        System.out.println(logBuilder.toString());
+                }
             }
+
         } catch (RuntimeException exception) {
             System.err.println("Uncaught exception on thread " + Thread.currentThread().getName() + ":");
             System.err.println(exception.getMessage());

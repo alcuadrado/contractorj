25d790080acb6c6cac9025b3f70f2e7858342a0a
Patricio Palladino <email@patriciopalladino.com>
2016-11-09 16:05:06 -0300

    Implemente Action's equals and hashCode

--- contractorj/src/main/java/contractorj/model/Action.java
+++ contractorj/src/main/java/contractorj/model/Action.java
@@ -2,6 +2,8 @@ package contractorj.model;
 
 import j2bpl.Method;
 
+import java.util.Objects;
+
 /**
  * An actions models a method with its precondition.
  */
@@ -22,4 +24,27 @@ public class Action {
 
         return method.getJavaName();
     }
+
+    @Override
+    public boolean equals(final Object o) {
+
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof Action)) {
+            return false;
+        }
+
+        final Action action = (Action) o;
+        return Objects.equals(precondition, action.precondition) &&
+                Objects.equals(method, action.method);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(precondition, method);
+    }
+
 }

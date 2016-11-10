c218c510dfbffffd8710490f3b3dae1b1e67242a
Patricio Palladino <email@patriciopalladino.com>
2016-11-10 20:53:32 -0300

    Move project to Java 8

--- j2bpl/src/main/java/j2bpl/Translator.java
+++ j2bpl/src/main/java/j2bpl/Translator.java
@@ -4,21 +4,22 @@ import soot.Pack;
 import soot.PackManager;
 import soot.Transform;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Translator {
 
-    public void translate(String pathToClassPath) {
+    public static final String JAVA_HOME_BIN_PATH = "/usr/libexec/java_home";
 
-        ensureJava7();
+    public void translate(String pathToClassPath) {
 
         Pack pack = PackManager.v().getPack("jtp");
 
         pack.add(new Transform("jtp.bpl", J2BplTransformer.getInstance()));
 
-        final String javaHome = System.getProperty("java.home");
-
-        final File rtFile = new File(new File(javaHome, "lib"), "rt.jar");
+        final File rtFile = new File(new File(new File(getJava7Home(), "jre"), "lib"), "rt.jar");
 
         final String[] args = {
                 "-keep-line-number",
@@ -40,21 +41,44 @@ public class Translator {
     }
 
     public Class getTranslatedClass(String className) {
+
         return J2BplTransformer.getInstance().getClass(className);
     }
 
     public String getTranslation() {
+
         return J2BplTransformer.getInstance().getTranslation();
     }
 
-    private static void ensureJava7() {
+    private String getJava7Home() {
 
-        final String implementationVersion = Runtime.class.getPackage().getImplementationVersion();
+        final File javaHomeBin = new File(JAVA_HOME_BIN_PATH);
 
-        if (!implementationVersion.startsWith("1.7")) {
-            throw new RuntimeException("J2Bpl can't be run with your jre because Soot doesn't support java >= 8.");
+        if (javaHomeBin.exists()) {
+            try {
+                final ProcessBuilder processBuilder = new ProcessBuilder(JAVA_HOME_BIN_PATH, "-v", "1.7*");
+                final Process process;
+                process = processBuilder.start();
+
+                final int statusCode = process.waitFor();
+
+                if (statusCode == 0) {
+                    return new BufferedReader(new InputStreamReader(process.getInputStream()))
+                            .readLine()
+                            .trim();
+                }
+            } catch (IOException | InterruptedException e) {
+                // Do nothing and try another method.
+            }
         }
-    }
 
+        final String java7Home = System.getenv("JAVA7_HOME");
+
+        if (java7Home != null) {
+            return java7Home;
+        }
+
+        throw new IllegalStateException("No Java 7 found. Try setting JAVA7_HOME env variable.");
+    }
 
 }

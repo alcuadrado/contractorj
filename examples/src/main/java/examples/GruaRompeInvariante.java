98019ff1e28571289fd03f5707d6d21a685a9c2c
Patricio Palladino <email@patriciopalladino.com>
2017-02-17 19:28:52 -0300

    Add examples baseed on Grua

---
+++ examples/src/main/java/examples/GruaRompeInvariante.java
@@ -0,0 +1,66 @@
+package examples;
+
+public class GruaRompeInvariante {
+
+  public boolean hayContainer;
+  public boolean containerCerrado;
+  public boolean containerVacio;
+
+  public GruaRompeInvariante() {
+    hayContainer = false;
+  }
+
+  public boolean inv() {
+    return !hayContainer || !(containerVacio && containerCerrado);
+  }
+
+  public boolean tomarContainer_pre() {
+    return !hayContainer;
+  }
+
+  public void tomarContainer() {
+    hayContainer = true;
+    containerCerrado = false;
+    containerVacio = true;
+  }
+
+  public boolean cargar_pre() {
+    return hayContainer && !containerCerrado;
+  }
+
+  public void cargar(int peso) {
+    containerVacio = false;
+  }
+
+  public boolean vaciar_pre() {
+    return hayContainer && !containerCerrado && !containerVacio;
+  }
+
+  public void vaciar() {
+    containerVacio = true;
+  }
+
+  public boolean cerrar_pre() {
+    return hayContainer && !containerCerrado;
+  }
+
+  public void cerrar() {
+    containerCerrado = true;
+  }
+
+  public boolean abrir_pre() {
+    return hayContainer && containerCerrado;
+  }
+
+  public void abrir() {
+    containerCerrado = false;
+  }
+
+  public boolean despachar_pre() {
+    return hayContainer && containerCerrado;
+  }
+
+  public void despachar() {
+    hayContainer = false;
+  }
+}

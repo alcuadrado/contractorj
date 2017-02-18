772ffe6a4cec6b85a3dd442caff52c85aa5294b8
Patricio Palladino <email@patriciopalladino.com>
2017-02-18 16:02:48 -0300

    Add GruaConAmbosDefectos example

---
+++ examples/src/main/java/examples/GruaAmbosDefectos.java
@@ -0,0 +1,70 @@
+package examples;
+
+public class GruaAmbosDefectos {
+
+  public boolean hayContainer;
+  public boolean containerCerrado;
+  public boolean containerVacio;
+
+  public GruaAmbosDefectos() {
+    hayContainer = false;
+  }
+
+  public boolean inv() {
+    return !hayContainer || !(containerVacio && containerCerrado);
+  }
+
+  public boolean tomarContainer_pre() {
+    return !hayContainer;
+  }
+
+  public void tomarContainer() {
+    hayContainer = true;
+    containerCerrado = false;
+    containerVacio = true;
+  }
+
+  public boolean cargar_pre() {
+    return hayContainer && !containerCerrado;
+  }
+
+  public void cargar(int peso) {
+    containerVacio = false;
+
+    if (peso > 1000) {
+      throw new RuntimeException("Carga demasiado pesada");
+    }
+  }
+
+  public boolean vaciar_pre() {
+    return hayContainer && !containerCerrado && !containerVacio;
+  }
+
+  public void vaciar() {
+    containerVacio = true;
+  }
+
+  public boolean cerrar_pre() {
+    return hayContainer && !containerCerrado;
+  }
+
+  public void cerrar() {
+    containerCerrado = true;
+  }
+
+  public boolean abrir_pre() {
+    return hayContainer && containerCerrado;
+  }
+
+  public void abrir() {
+    containerCerrado = false;
+  }
+
+  public boolean despachar_pre() {
+    return hayContainer && containerCerrado;
+  }
+
+  public void despachar() {
+    hayContainer = false;
+  }
+}

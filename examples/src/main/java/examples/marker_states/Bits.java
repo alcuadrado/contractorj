68d8d174341a233876b451f2870dd13c9817c39e
m7nu3l <mcarrasco@dc.uba.ar>
2017-07-06 13:27:15 -0300

    Added case for analysis. It can be compiled. It cannot be translated yet.

---
+++ examples/src/main/java/examples/marker_states/Bits.java
@@ -0,0 +1,15 @@
+package examples.marker_states;
+
+/**
+ * Created by Usuario on 04/07/2017.
+ */
+public class Bits {
+
+    static ByteOrder byteOrder() {
+        //if (byteOrder == null)
+        //    throw new Error("Unknown byte order");
+        //return byteOrder;
+        return ByteOrder.BIG_ENDIAN; // ESTO ESTA MAL HAY QUE HACERLO NO DETERMINISTICO CON LITTLE
+    }
+
+}
